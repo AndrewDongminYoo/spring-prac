af2505f8bd902e03768abad8e3c97e2bf3bf436c
AndrewDongminYoo <ydm2790@gmail.com>
2021-11-04 21:17:33 +0900

    Initial commit

---
+++ src/main/java/com/sparta/feed24/Feed24Application.java
@@ -0,0 +1,13 @@
+package com.sparta.feed24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feed24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Feed24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/feed24/Feed24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.feed24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feed24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
